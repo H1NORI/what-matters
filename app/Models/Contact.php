b37ea0e3b5a637382325ada60f2ba10cc81fb4b1
Nikita Tkachuk <nnikkito019@gmail.com>
2026-07-01 01:43:30 +0300

    added contact modal

---
+++ app/Models/Contact.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    /** @use HasFactory<\Database\Factories\ContactFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'owner_id',
+        'user_id',
+        'name',
+        'birthdate',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
